d541fd3ab9c7a8236fce1829d11c18435f0b72f4
Duleesha <duleeshajayangani29@gmial.com>
2025-04-02 21:46:20 +0530

    init👍

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <div>
+      <ProductCard name="Audio Setup" price="3250/="
+      description="loreubbd dbbdbbd ddd"
+      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWyw8yNk0Z3MLY53dzlFRNkv-t4ZaMScS_3mBJYVyHeRnegLW-rDCi7SqLCeW92quuRes&usqp=CAU"/>
+      
+      <ProductCard name="Audio Setup" price="3250/="
+      description="loreubbd dbbdbbd ddd"
+      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWyw8yNk0Z3MLY53dzlFRNkv-t4ZaMScS_3mBJYVyHeRnegLW-rDCi7SqLCeW92quuRes&usqp=CAU"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,15 @@
+import "./productCard.css";
+export default function ProductCard(props){
+
+
+
+return(
+    <div>
+        <img src={props.img}/>
+        <span>{props.name}</span>
+        <span>LKR.{props.price}</span>
+        <p>{props.description}</p>
+    </div>
+)
+
+}
