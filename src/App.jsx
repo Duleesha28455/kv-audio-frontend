39403b337ba424050e27c3ad8dfc7cd25be9e577
Duleesha <duleeshajayangani29@gmial.com>
2025-04-07 12:00:50 +0530

    day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { MdOutlineSpeaker } from "react-icons/md";
 import "./App.css";
 import ProductCard from "./components/productCard";
-import { BsGraphDown } from "react-icons/bs";
-import { FaRegBookmark, FaRegUser } from "react-icons/fa";
+import { BsGraphUp } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa6";
+import { LuSpeaker } from "react-icons/lu";
+import { FaRegUser } from "react-icons/fa6";
+
 function App() {
   return (
-    <div className="w-full h-screen flex">
+   
+      <div className='w-full h-screen flex'>
       <div className="w-[400px] h-full bg-green-200">
-        <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center items-center">
-          <BsGraphDown/>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+        <BsGraphUp />
           Dashboard
         </button>
-        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
-          <FaRegBookmark/>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center ">
+        <FaRegBookmark />
           Bookings
         </button>
         <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
-          <MdOutlineSpeaker/>
+          <LuSpeaker/>
           Items
         </button>
         <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
-          <FaRegUser/>
+        <FaRegUser />
           Users
         </button>
 
       </div>
       <div className="w-full bg-red-900">
       
-
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,14 +1,19 @@
 import "./productCard.css";
-export default function ProductCard(props){
-
+  export default function ProductCard(props){
   console.log(props)
+
+
 
   return(
     <div>
       <img src={props.photoUrl}/>
       <span>{props.name}</span>
-      <span>LKR. {props.price}</span>
-      <p>{props.description}</p>
+      <span>
+        LKR. {props.price}
+      </span>
+      <p>
+         {props.description}
+      </p>
     </div>
   )
 }
